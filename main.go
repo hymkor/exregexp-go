4918a342bbf2f7202a56de2c67b133e3a0671fc3
HAYAMA_Kaoru <3752189+hymkor@users.noreply.github.com>
2025-05-29 08:58:13 +0900

    Implement the bytes version: ReplaceAllSubmatchFunc

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package exregexp
 
 import (
+	"bytes"
 	"strings"
 )
 
-type Regexp interface {
+type RegexpBin interface {
+	FindAllSubmatchIndex([]byte, int) [][]int
+}
+
+func ReplaceAllSubmatchFunc(rx RegexpBin, input []byte, f func([][]byte) []byte) []byte {
+	var result bytes.Buffer
+
+	matches := rx.FindAllSubmatchIndex(input, -1)
+	lastIndex := 0
+	for _, match := range matches {
+		result.Write(input[lastIndex:match[0]])
+
+		sub := make([][]byte, len(match))
+		for i := 0; i*2 < len(match); i++ {
+			if from := match[i*2]; from < 0 {
+				sub[i] = []byte{}
+			} else if to := match[i*2+1]; to < 0 {
+				sub[i] = []byte{}
+			} else {
+				sub[i] = input[from:to]
+			}
+		}
+		result.Write(f(sub))
+
+		lastIndex = match[1]
+	}
+	result.Write(input[lastIndex:])
+	return result.Bytes()
+}
+
+type RegexpStr interface {
 	FindAllStringSubmatchIndex(string, int) [][]int
 }
 
@@ -12,7 +43,7 @@ type Regexp interface {
 // matches of the regular expression in the input string. The replacement
 // function receives a slice of submatches, where the first element is the
 // entire match and subsequent elements are the capturing groups.
-func ReplaceAllStringSubmatchFunc(rx Regexp, input string, f func([]string) string) string {
+func ReplaceAllStringSubmatchFunc(rx RegexpStr, input string, f func([]string) string) string {
 	var result strings.Builder
 
 	matches := rx.FindAllStringSubmatchIndex(input, -1)
--- main_test.go
+++ main_test.go
@@ -2,9 +2,79 @@ package exregexp
 
 import (
 	"fmt"
+	"bytes"
 	"regexp"
 	"testing"
 )
+
+func concat(s ...[]byte) []byte {
+	r := []byte{}
+	for _, s1 := range s {
+		r = append(r, s1...)
+	}
+	return r
+}
+
+func TestReplaceAllSubmatchFunc(t *testing.T) {
+	rx, err := regexp.Compile(`\[\[(?:([^\|\]]+)\|)?(.+?)\]\]`)
+	if err != nil {
+		t.Fatalf("test regexp error: %s", err.Error())
+	}
+
+	result := ReplaceAllSubmatchFunc(rx,
+		[]byte("foo[[LINKED TEXT|URL]]bar"),
+		func(s [][]byte) []byte {
+			return []byte(fmt.Sprintf(`<a href="%s">%s</a><!-- %s -->`, string(s[2]), string(s[1]), string(s[0])))
+		})
+
+	expect := []byte(`foo<a href="URL">LINKED TEXT</a><!-- [[LINKED TEXT|URL]] -->bar`)
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("(1) expect `%v`, but `%v`", expect, result)
+	}
+
+	result = ReplaceAllSubmatchFunc(rx,
+		[]byte("foo[[LINKED TEXT|URL]]bar[[linked text|url]]qux"),
+		func(s [][]byte) []byte {
+			return []byte(fmt.Sprintf(`<a href="%s">%s</a><!-- %s -->`, string(s[2]), string(s[1]), string(s[0])))
+		})
+	expect = []byte(`foo<a href="URL">LINKED TEXT</a><!-- [[LINKED TEXT|URL]] -->bar<a href="url">linked text</a><!-- [[linked text|url]] -->qux`)
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("(2) expect `%v`, but `%v`", expect, result)
+	}
+
+	result = ReplaceAllSubmatchFunc(rx,
+		[]byte("A[[1|2]]B[[3|4]]C[[5|6]]D"),
+		func(s [][]byte) []byte {
+			return []byte(fmt.Sprintf(`[%s](%s)<!-- %s -->`, string(s[1]), string(s[2]), string(s[0])))
+		})
+	expect = []byte(`A[1](2)<!-- [[1|2]] -->B[3](4)<!-- [[3|4]] -->C[5](6)<!-- [[5|6]] -->D`)
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("(2) expect `%v`, but `%v`", expect, result)
+	}
+
+	rx, err = regexp.Compile(`([a-zA-Z]+)([0-9])?`)
+	if err != nil {
+		t.Fatalf("test regexp error: %s", err.Error())
+	}
+	result = ReplaceAllSubmatchFunc(rx,
+		[]byte("aiueo1"),
+		func(s [][]byte) []byte {
+			return concat(s[1], []byte{'-'}, s[2])
+		})
+	expect = []byte("aiueo-1")
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("(3) expect `%v`, but `%v`", expect, result)
+	}
+	result = ReplaceAllSubmatchFunc(rx,
+		[]byte("aiueo"),
+		func(s [][]byte) []byte {
+			return concat(s[1], []byte{'-'}, s[2])
+		})
+	expect = []byte("aiueo-")
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("(4) expect `%v`, but `%v`", expect, result)
+	}
+}
 
 func TestReplaceAllStringSubmatchFunc(t *testing.T) {
 	rx, err := regexp.Compile(`\[\[(?:([^\|\]]+)\|)?(.+?)\]\]`)
